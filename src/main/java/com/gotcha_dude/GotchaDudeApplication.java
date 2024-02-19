f31b3ad9292cb35dcd17f5bb9431ad3b629d80d9
KKchan1119 <kkchan1119@gmail.com>
2024-02-19 22:58:00 +0900

    setup

---
+++ src/main/java/com/gotcha_dude/GotchaDudeApplication.java
@@ -0,0 +1,13 @@
+package com.gotcha_dude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotchaDudeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GotchaDudeApplication.class, args);
+    }
+
+}
